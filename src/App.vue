bdee6753ac54218130e78d61babe914bddf5d862
arifhusaini97 <chemist.arif.husaini@gmail.com>
2020-12-15 14:18:59 +0800

    Cleaning 2
    Cleaning again.

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,10 @@
   <!--<img alt="Vue logo" src="./assets/logo.png">-->
   <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
   <!--<NavBar />-->
-  <SideBar />
-  
-  <div class="main"><router-view/> </div>
-  
+  <div class="sidebar"><SideBar /></div>
+
+  <div class="main"><router-view /></div>
+
   <!--<div class="main">testing.qwertyuiop</div>-->
 </template>
 
@@ -26,13 +26,28 @@ export default {
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  
+
   // font-family: 'Roboto', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   min-height: 100vh;
+
+  .sidebar {
+    height: 100%; /* Full-height: remove this if you want "auto" height */
+    width: 170px; /* Set the width of the sidebar */
+    position: fixed; /* Fixed Sidebar (stay in place on scroll) */
+    // z-index: 1; /* Stay on top */
+    // top: 0; /* Stay at the top */
+    // left: 0;
+    background-color: white; /* Black */
+    // overflow-x: hidden; /* Disable horizontal scroll */
+    flex-direction: column;
+    border-right: double;
+    border-right-width: 5px;
+    border-right-style: solid;
+  }
 
   /* Style page content */
   .main {
--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,6 +1,6 @@
 <template>
+  <div class="logo">ArifAlgo</div>
   <div class="sidenav">
-    <div class="logo">ArifAlgo</div>
     <router-link to="/dashboard">Dashboard</router-link>
     <router-link to="/arts">Arts</router-link>
     <router-link to="/learns">Learns</router-link>
@@ -15,27 +15,15 @@ export default {
 
 <style lang="scss" scoped>
 /* The sidebar menu */
+
+.logo {
+  padding: 5px 5px 5px 5px;
+  font-size: 35px;
+  font-weight: bold;
+}
+
 .sidenav {
-  height: 100%; /* Full-height: remove this if you want "auto" height */
-  width: 170px; /* Set the width of the sidebar */
-  position: fixed; /* Fixed Sidebar (stay in place on scroll) */
-  // z-index: 1; /* Stay on top */
-  // top: 0; /* Stay at the top */
-  // left: 0;
-  background-color: white; /* Black */
-  // overflow-x: hidden; /* Disable horizontal scroll */
-  flex-direction: column;
-  border-right: double;
-  border-right-width: 5px;
-  border-right-style: solid;
-
   /* The navigation menu links */
-
-  .logo{
-    padding: 5px 5px 5px 5px;
-    font-size: 35px;
-    font-weight: bold;
-  }
 
   a {
     padding: 7px 7px 7px 7px;
